7e9eeffb73263a4442bc8ff617bb2fd9e137f1a6
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-09-26 15:17:06 -0500

    [add] input tag and create javascript file

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+  const amenities = {};
+  $('li input[type="checkbox"]').change(function () {
+    /* add elements to amenities dictionary */
+    if ($(this).prop('checked')) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    /* get elements from amenities dictionary */
+    if (Object.keys(amenities).length === 0) {
+      $('.amenities h4').html('&nbsp;');
+    } else {
+      $('.amenities h4').text(Object.values(amenities).join(', '));
+    }
+  });
+  $('li input[type="checkbox"]').css('margin-right', '10px');
+});
